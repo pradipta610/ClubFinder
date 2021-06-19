cff22fff05bb11ba83724136ed0dfc9220cee0a4
Pradipta610 <pradipta.wistika@gmail.com>
2021-06-19 10:53:01 +0800

    club finder dicoding upload

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import "regenerator-runtime/runtime.js";
+import "./styles/style.css";
+import "./script/component/app-bar.js";
+import main from "./script/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
